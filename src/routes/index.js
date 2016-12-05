926490651a053d49448bc3742d54dd04bfa2252a
Artis Coleman <ArtisColemanJr@gmail.com>
2016-12-04 22:45:13 -0500

    Finished creating beginner portion of the Foodtruck API

---
+++ src/routes/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import config from '../config';
+import middleware from '../middleware';
+import initializeDb from '../db';
+import foodtruck from '../controller/foodtruck';
+import account from '../controller/account';
+
+let router = express();
+
+// Connect to db
+initializeDb(db => {
+
+    // Internal middleware
+    router.use(middleware({config, db}));
+
+    // API routes v1 (/v1)
+    router.use('/foodtruck', foodtruck({config, db}));
+    router.use('/account', account({config, db}));
+
+})
+
+export default router;
